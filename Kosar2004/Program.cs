9333aee6763ecb4b544292788350fbdc6bca2711
kovacsgaborab <kovacsgaborab@gmail.com>
2020-10-15 09:20:04 +0200

    Meccs osztály elkészítése

--- Kosar2004/Program.cs
+++ Kosar2004/Program.cs
@@ -8,8 +8,18 @@ namespace Kosar2004
 {
     class Program
     {
+        static List<Meccs> Meccsek = new List<Meccs>();
         static void Main(string[] args)
         {
+            //7up Joventut; Adecco Estudiantes; 81; 73; Palacio Mun. De Deportes De Badalona; 2005 - 04 - 03
+
+
+
+            var m = new Meccs("7up Joventut", "Adecco Estudiantes", 81, 73, "Palacio Mun. De Deportes De Badalona", "2005 - 04 - 03");
+
+            Console.WriteLine($"{m.Hazai} - {m.Idegen} ({m.HPont}:{m.IPont})");
+
+            Console.ReadLine();
         }
     }
 }
